bef79f539ba4c03fab3af9626e639fc025413f08
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-07-08 00:49:33 -0400

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "rays.h"
+
+using namespace RayTracer;
+
+constexpr double aspect_ratio {16.0/9.0};
+constexpr int image_width {1200};
+constexpr int image_height = static_cast<int>(image_width/aspect_ratio);
+
+int main() {
+   
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    //viewport setup
+    double viewportHeight = 2.0;
+    double viewportWidth = viewportHeight * aspect_ratio;
+    double focalLength = 1.0;
+    Point origin{0,0,0};
+    Vector horizontal {viewportWidth,0,0};
+    Vector vertical {0,viewportHeight,0};
+    Vector lower_left_corner = origin - horizontal/2 - vertical/2 - Vector{0, 0, focalLength};
+    //loop for rows
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        //loop for pixels
+        for (int i = 0; i < image_width; ++i) {
+            double u = double(i) / (image_width-1);
+            double v = double(j) / (image_height-1);
+            // setup a color gradient
+            Ray scene(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            Color pixelColor = rayColor(scene);
+            writeColor(std::cout, pixelColor);
+
+        }
+    }
+    
+    return 0;
+}
