b387cf4f2a8505455ad5933a012f3fac5ae8f8bc
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2020-07-21 15:29:26 -0400

    Update helper.h

--- inc/helper.h
+++ inc/helper.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <cstdlib>
 #include <cmath>
 #include <limits>
 
@@ -13,17 +14,22 @@ using std::string;
 using std::unique_ptr;
 using std::shared_ptr;
 
-//constants
-constexpr double aspect_ratio {16.0/9.0};
-constexpr int image_width {1200};
-constexpr int image_height = static_cast<int>(image_width/aspect_ratio);
-constexpr double pi = 3.14159265;
-constexpr double inf = std::numeric_limits<double>::infinity();
+namespace RayTracer
+{
+    //constants
+    constexpr double aspect_ratio {16.0/9.0};
+    constexpr int image_width {1200};
+    constexpr int image_height = static_cast<int>(image_width/aspect_ratio);
+    constexpr double pi = 3.14159265;
+    constexpr double inf = std::numeric_limits<double>::infinity();
+    constexpr int pixelSamples{100};
+    constexpr int maxDepth{50};
 
-//functions
-inline double degToRad(double degreeAngle){
-    return (pi/180)*degreeAngle;
-}
-inline double radtoDeg(double radAngle) {
-    return (180/pi) * radAngle;
-}
+    //functions
+    double degToRad(double degreeAngle);
+    double radtoDeg(double radAngle);
+    double randomDecimal();
+    double randomDecimal(double , double );
+    double clipRange(double val, double min, double max);
+
+} //namespace RayTracer
